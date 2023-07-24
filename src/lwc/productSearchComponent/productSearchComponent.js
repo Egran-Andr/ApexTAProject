8d69d77f85ed1fd61f409f94abad3fd3469a3c7c
Egran-Andr <mlgrainbowfrog2003@gmail.com>
2023-07-24 23:49:17 +0300

    Created variant of shopping cart cass with wrapper

--- src/lwc/productSearchComponent/productSearchComponent.js
+++ src/lwc/productSearchComponent/productSearchComponent.js
@@ -2,15 +2,20 @@
  * Created by Egran on 23.07.2023.
  */
 
+
 import { LightningElement,wire,api } from 'lwc';
 import getProductRecords from '@salesforce/apex/ObjectGetter.getProductRecords';
+import addToCart from '@salesforce/apex/UserProductCart.addToCart';
+import getTotalCost from '@salesforce/apex/UserProductCart.getTotalCost';
+
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ProductSearchComponent extends LightningElement {
     openModal = false;
     products=[];
-    itemName="TestTest";
-    clickedButtonLabel;
+    itemName;
     itemDescryption;
+    checkCost;
     @wire(getProductRecords)
         productrecords({ data, error  }) {
         if (data) {
@@ -23,6 +28,39 @@ export default class ProductSearchComponent extends LightningElement {
     handleInputChange(event) {
     this.clickedButtonLabel = event.target.value;
     }
+
+    addToCart(event) {
+        this.itemName=event.target.value;
+        addToCart({productname: this.itemName,productcost:2000})
+              .then((result) => {
+                    const evt = new ShowToastEvent({
+                        title: 'Added to cart',
+                        message:'Product was successfully added to cart.',
+                        variant: 'success',
+                    });
+                    this.dispatchEvent(evt);
+                    getTotalCost()
+                          .then((result) => {
+                            this.checkCost = result.toString();
+                          })
+                          .catch((error) => {
+                            this.error = error;
+                          });
+              })
+              .catch((error) => {
+                this.error = error;
+                this.contacts = undefined;
+                                    const evt = new ShowToastEvent({
+                                        title: 'Error',
+                                        message:'Unexpected error. Please try again',
+                                        variant: 'error',
+                                    });
+                                    this.dispatchEvent(evt);
+              });
+
+
+    }
+
 
     openDetails(event){
     this.openModal = true;
