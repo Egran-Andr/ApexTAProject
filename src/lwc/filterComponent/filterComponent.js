153fc07b58fbbf26b190867dc2a3f40b88e1245e
Egran-Andr <mlgrainbowfrog2003@gmail.com>
2023-07-24 03:01:49 +0300

    Created html combobox generator for filterComponent page

--- src/lwc/filterComponent/filterComponent.js
+++ src/lwc/filterComponent/filterComponent.js
@@ -2,22 +2,36 @@
  * Created by Egran on 23.07.2023.
  */
 
-import { LightningElement,wire,track } from 'lwc';
+import { LightningElement,wire,track,api  } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getTypesPickListValuesIntoList from '@salesforce/apex/ObjectGetter.getTypesPickListValuesIntoList';
+import getFamilyPickListValuesIntoList from '@salesforce/apex/ObjectGetter.getFamilyPickListValuesIntoList';
 
 export default class FilterComponent extends LightningElement {
-        @track typesize;
+     types=[];
+     families=[];
+    clickedButtonLabel;
         @wire(getTypesPickListValuesIntoList)
-        getprediction({ data, error  }) {
+        producttypes({ data, error  }) {
             if (data) {
-            for (let i = 0; i < data.length; i++) {
-
-            }
+            this.types=data;
             } else if (error) {
                 this.error = error;
             }
         }
+        ///## (BUG)repopulating HTML code lead to error 957897146, reasons unknown but it make objects appear and function normally,but it appears to not change their behavior
+      @wire(getFamilyPickListValuesIntoList)
+           familytypes({ data, error  }) {
+               if (data) {
+               this.families=data;
+               } else if (error) {
+                   this.error = error;
+               }
+           }
+
+    handleChange(e) {
+        this.clickedButtonLabel = e.target.name;
+    }
 
 
 }
